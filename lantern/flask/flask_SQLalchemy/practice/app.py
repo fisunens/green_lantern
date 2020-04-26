95b9a724651a215db69850326684d78d645751db
fisunens <fisunens@gmail.com>
2020-04-26 22:14:37 +0300

    Create app.py

--- lantern/flask/flask_SQLalchemy/practice/app.py
+++ lantern/flask/flask_SQLalchemy/practice/app.py
@@ -2,22 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import create_database, drop_database, database_exists
 
-from config import Config
-from populate_data import get_users
+class Config:
+    PG_USER = "cursor"
+    PG_PASSWORD = "very_secret_password"
+    PG_HOST = "localhost"
+    PG_PORT = 5432
+    DB_NAME = "cursor_sqlalchemy_db"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{DB_NAME}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 db = SQLAlchemy()
-
-
-class User(db.Model):
-    __tablename__ = "users"
-
-    user_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
-    email = db.Column(db.String(), nullable=False)
-    password = db.Column(db.String(), nullable=False)
-
-
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
@@ -25,15 +20,7 @@ db.init_app(app)
 with app.app_context():
     if database_exists(db.engine.url):
         db.create_all()
-        print('Database exists')
+        print('database exists')
     else:
-        print(f"Database does not exists {db.engine.url}")
-        create_database(db.engine.url)
-        print('Data base created')
+        print('database does not exists')
 
-with app.app_context():
-    users = get_users()
-    for user in users:
-        db.session.add(User(**user))
-    db.session.commit()
-    print('Data written in data_base succesfuly')
